24ffa22e0fc4bf73d4238f7f419c35a757951ee7
SooLight <soso1461@naver.com>
2024-07-02 19:44:59 +0900

    back init

---
+++ src/main/java/com/quiz/quizback/QuizbackApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.quizback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbackApplication.class, args);
+	}
+
+}
